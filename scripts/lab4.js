5ed23963618f122dc89b49795fcd27caced653d4
Teá Ruiz <122774384+tearuiz@users.noreply.github.com>
2025-04-28 17:52:45 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,12 +6,15 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return false;
+    }
     if (add) {
 
         return num1 + num2;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -22,6 +25,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || typeof discount !== 'number') {
+    return false;
+}
     if(prices.length==0){
         return false;
     }
