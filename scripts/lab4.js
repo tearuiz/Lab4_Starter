3a9a40a278aff1c4f6e6630c82ac3c429db0a9b0
Teá Ruiz <122774384+tearuiz@users.noreply.github.com>
2025-04-28 17:46:55 -0700

    update to pass test

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -27,7 +27,7 @@ function discountPrices(prices, discount) {
     }
     const discounted = []
     for(let i = 0; i < prices.length; i++) {
-        let discountedPrice += prices[i] * (1 - discount);
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
